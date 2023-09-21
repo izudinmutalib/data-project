dfc3694227b60e2701954f901ca10832d95d99fb
izudinmutalib <izudinmutalib@gmail.com>
2023-09-21 17:00:28 +0000

    update

--- profiling.py
+++ profiling.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-df = pd.read_csv('')
-profile_report = ProfileReport(df, title='Animal Data Profiling')
+df = pd.read_csv('spotify_most_streamed.csv')
+profile_report = ProfileReport(df, title='Spotify Data Profiling')
+profile_report.to_file('report.html')
