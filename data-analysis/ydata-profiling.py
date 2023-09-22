8f5d2a6bb2c806a444f567f7a183b4c0fa276089
Muhd Izudin <izudinmutalib@gmail.com>
2023-09-22 02:16:41 +0000

    update with notes

---
+++ data-analysis/ydata-profiling.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+df = pd.read_csv('/workspace/data-project/data-analysis/spotify_most_streamed.csv')
+profile_report = ProfileReport(df, title='First Data Profiling', explorative=True)
+
+df = pd.read_csv('/workspace/data-project/data-analysis/spotify_most_streamed_copy.csv')
+profile2_report = ProfileReport(df, title='Second Data Profiling', explorative=True)
+
+comparison_report = profile_report.compare(profile2_report)
+comparison_report.to_file('compare.html')
